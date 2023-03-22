d6bca6f93e1eaa08a6b44c60b89c956002fd6ad3
Juan Pedro Medeiros <jpedro.med@hotmail.com>
2023-03-22 20:27:21 -0300

    Criação do projeto e primeiros ajustes

---
+++ SistemaAspNetMVC/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SistemaAspNetMVC.Models;
+using System.Diagnostics;
+
+namespace SistemaAspNetMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
